252da8d33f5bdede874baeb049cd40220b50f16a
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-07-05 21:26:46 -0400

    API fetch success

---
+++ finnhub_client.py
@@ -0,0 +1,23 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.environ["FINNHUB_API_KEY"]
+BASE_URL = "https://finnhub.io/api/v1"
+
+
+def get_news(category="general"):
+    url = f"{BASE_URL}/news?category={category}&token={API_KEY}"
+    
+    response = requests.get(url)
+    data = response.json()
+
+    print(data[:3])
+   
+    return data
+
+
+
+get_news()
