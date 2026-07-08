3f5a3122fb1bbb9518fdbc45a361cda001e92787
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-07-08 03:27:00 -0400

    updated using scheduler APS

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from summary import summarize_news, filter_articles
 from fastapi import FastAPI, HTTPException
 from db import create_table, replace_articles, get_articles
 from contextlib import asynccontextmanager
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
 
+
+
+scheduler = BackgroundScheduler()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_table()
+    scheduler.add_job(refresh_news, trigger=CronTrigger(hour="8,10,12,14,16,18,20"))
+    scheduler.start()
     yield
 
 
@@ -17,7 +24,7 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/news-getter")
-def get_news():
+def refresh_news():
     try:
         articles = get_news()
         filtered_articles = filter_articles(articles)  
